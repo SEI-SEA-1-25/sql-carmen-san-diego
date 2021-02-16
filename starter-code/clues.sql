ddf7b5a79fbbc291ae12ac7b77da8c2c41791067
CHRISDAVIDSPICER <CHRISDAVIDSPICER@GMAIL.COM>
2021-02-16 15:59:46 -0800

    Change one line

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -25,7 +25,7 @@ SELECT name FROM cities WHERE countrycode = 'SMR';
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-SELECT name, countrycode FROM cities WHERE name = 'Serra';
+SELECT name, countrycode FROM cities WHERE name LIKE 'Ser%';
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
