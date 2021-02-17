6efc72a1b8b240904f5c0cefa9c425925763b770
Darren <73214654+darrengp@users.noreply.github.com>
2021-02-16 16:14:12 -0800

    Finished

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -35,13 +35,15 @@
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
-
+-- Answer
+-- Serra
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
-
+-- Answer
+-- 211 Brasila
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
@@ -61,4 +63,4 @@
 
 
 
--- She's in ____________________________!
+-- She's in _____Santa Monica_______________________!
